9e1e17f7fbc1fc36b3c4f17de9dcd1eeaf66ff9e
Adriano Dutra <adriano@teleworld.com.br>
2023-10-03 12:33:34 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -279,8 +279,6 @@ export default class PN532 extends EventEmitter {
             const _options = this.options;
             if (!this.isOpen && this.port && this.port.isOpen) {
                 if (!_options.baudRate) {
-                    //await this.findBaudRate();
-                    await this.powerDown();
                     await this.setBaudRate(EBaudRates.BR230400);
                 }
                 await this.powerDown();
